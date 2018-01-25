789ffc66c740377973db67591b4476e41f960603
Utz Stauder <utzstauder@users.noreply.github.com>
2018-01-25 11:25:24 +0100

    Improved random power up spawning

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -24,21 +24,33 @@ public class Brick : MonoBehaviour {
         // spawn power up
         if (powerupPrefabs != null)
         {
-            // instantiate new gameobject
-            GameObject powerup = Instantiate(powerupPrefabs[Random.Range(0, powerupPrefabs.Length)], transform.position, Quaternion.identity);
-            // GameObject powerup = Instantiate(gameObject, Random.insideUnitSphere * Random.Range(0, 4f), Quaternion.identity);
-
-            // find DynamicObjects
-            GameObject dynamicObjects = GameObject.Find("DynamicObjects");
-
-            // no DynamicObjects? create it!
-            if (dynamicObjects == null)
+            if (powerupPrefabs.Length > 0)
             {
-                dynamicObjects = new GameObject("DynamicObjects");
-            }
+                // get random index
+                int randomIndex = Random.Range(0, powerupPrefabs.Length);
 
-            // parent instantiated object to DynamicObjects gameobject
-            powerup.transform.parent = dynamicObjects.transform;
+                // get random powerup prefab
+                GameObject powerupPrefab = powerupPrefabs[randomIndex];
+
+                if (powerupPrefab != null)
+                {
+                    // instantiate new gameobject
+                    GameObject powerup = Instantiate(powerupPrefab, transform.position, Quaternion.identity);
+                    // GameObject powerup = Instantiate(gameObject, Random.insideUnitSphere * Random.Range(0, 4f), Quaternion.identity);
+
+                    // find DynamicObjects
+                    GameObject dynamicObjects = GameObject.Find("DynamicObjects");
+
+                    // no DynamicObjects? create it!
+                    if (dynamicObjects == null)
+                    {
+                        dynamicObjects = new GameObject("DynamicObjects");
+                    }
+
+                    // parent instantiated object to DynamicObjects gameobject
+                    powerup.transform.parent = dynamicObjects.transform;
+                }
+            }
         }
 
         Destroy(gameObject);
