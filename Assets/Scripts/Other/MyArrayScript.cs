c2d332ed916c07c4a8cec5d074a63b492767afac
Utz Stauder <utzstauder@users.noreply.github.com>
2018-01-25 11:11:46 +0100

    Added Sum() and Average() functions

--- Assets/Scripts/Other/MyArrayScript.cs
+++ Assets/Scripts/Other/MyArrayScript.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
+using System.Linq;
 
 public class MyArrayScript : MonoBehaviour {
+
+    public int randomArrayLength = 100;
 
     int number = 10;
 
@@ -8,9 +11,12 @@ public class MyArrayScript : MonoBehaviour {
 
     int[] numbers = new int[3];
 
-    int[] otherNumbers = new int[] { 1, 2, 3, 4, 5, 10, 20 };
+    int[] otherNumbers = new int[] { 1, 2000, 3, 4, 5, 10, 20 };
 
 	void Start () {
+        MyPrint("Hello", "World", "What's up?");
+        MyPrint("Yo!");
+
         Debug.LogFormat("number = {0}", number);
 
         Debug.LogFormat("empty = {0}", empty);
@@ -24,17 +30,57 @@ public class MyArrayScript : MonoBehaviour {
             Debug.Log(otherNumbers[i]);
         }
 
-        // assign values like this
-        numbers[3] = 10;
 
-        for (int i = 0; i < 100; i++)
+        int[] randomNumbers = new int[randomArrayLength];
+
+        for (int i = 0; i < randomArrayLength; i++)
         {
             // get "random" index
             int randomIndex = Random.Range(0, otherNumbers.Length);
-            Debug.LogFormat("random index = {0}, element value = {1}",
-                            randomIndex,
-                            otherNumbers[randomIndex]);
+            //Debug.LogFormat("random index = {0}, element value = {1}",
+            //                randomIndex,
+            //                otherNumbers[randomIndex]);
+            randomNumbers[i] = otherNumbers[randomIndex];
         }
+
+        // calculate average
+        float randomAverage = Average(randomNumbers);
+        randomAverage = (float)randomNumbers.Average();
+        float actualAverage = Average(otherNumbers);
+
+        float someAverage = Average(2, 50, 200, -10);
+
+        Debug.LogFormat("random average = {0} | actual average = {1}", randomAverage, actualAverage);
 	}
 	
+    int Sum(params int[] array)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+
+    float Average(params int[] array)
+    {
+        float average = 0;
+
+        int sum = Sum(array);
+        average = (float)sum / array.Length;
+
+        return average;
+    }
+
+
+    void MyPrint(params string[] text)
+    {
+        for(int i = 0; i < text.Length; i++)
+        {
+            Debug.Log(text[i]);
+        }
+    }
 }
