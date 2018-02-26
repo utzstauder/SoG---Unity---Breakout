bbea85e4c3e6a30499dfc73ba7981011142feba5
Utz Stauder <utzstauder@users.noreply.github.com>
2018-02-26 13:43:34 +0100

    Array sort

---
+++ Assets/Scripts/ArraySort/MyArraySort.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyArraySort : MonoBehaviour {
+
+	void Start () {
+		// 1) get random array
+
+        // 2) output original array
+
+        // 3) sort array
+
+        // 4) output sorted array
+	}
+
+    /// <summary>
+    /// Swaps two elements in array <paramref name="array"/>
+    /// </summary>
+    /// <param name="array">The array</param>
+    /// <param name="a">Index of first element</param>
+    /// <param name="b">Index of second element</param>
+    void Swap(int[] array, int a, int b)
+    {
+
+    }
+
+    /// <summary>
+    /// Returns an array with length <paramref name="length"/>
+    /// filled with random integer values.
+    /// </summary>
+    /// <param name="length"></param>
+    /// <returns></returns>
+    int[] GetRandomArray(int length)
+    {
+
+    }
+
+}
