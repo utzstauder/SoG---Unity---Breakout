cef9fcc51c80aff80e00c982dda81a7f495b2dac
Utz Stauder <utzstauder@users.noreply.github.com>
2018-03-12 09:49:58 +0100

    out, ref

---
+++ Assets/Scripts/ActionRPG/ActionRPGController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionRPGController : MonoBehaviour {
+
+    private void Start()
+    {
+        int a = 10;
+        int b = 20;
+        Debug.LogFormat("a = {0}, b = {1}", a, b);
+
+        int c;
+        Add(a, b, out c);
+        Debug.LogFormat("c = {0}", c);
+    }
+
+    int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    void Add(int a, int b, out int result)
+    {
+        result = a + b;
+    }
+
+
+
+
+    void Update () {
+
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(ray, out hitInfo, float.MaxValue))
+            {
+                Debug.LogFormat("Ray hit {0} at {1}",
+                    hitInfo.transform.gameObject.name,
+                    hitInfo.point);
+            }
+        }
+	}
+}
