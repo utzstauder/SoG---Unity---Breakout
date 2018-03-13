73276ffb687ffdbf8640a5f908312bc2e1f262e6
Utz Stauder <utzstauder@users.noreply.github.com>
2018-03-13 10:44:20 +0100

    animation controller part 1

---
+++ Assets/Scripts/AnimationScripts/PlayerAnimationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour {
+
+    Animator animator;
+
+	void Awake () {
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+        animator.SetFloat("vHorizontal", Input.GetAxisRaw("Horizontal"));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("attack");
+        }
+	}
+}
